2e7957c9ca9dfb5fd5a6a91ed856a6123133b3b1
Bastien B <bastien-bouge@hotmail.fr>
2022-04-21 18:00:01 +0200

    f

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on('ready', () => {
 client.on('interactionCreate', (interaction) => {
 
     if (!interaction.isCommand()) return;
-    if (!config.adminIDs.includes(interaction.user.id)) return void interaction.reply(`:x: Vous ne disposez pas des droits pour effectuer cette action !`);
+    if (!process.env.ADMIN.includes(interaction.user.id)) return void interaction.reply(`:x: Vous ne disposez pas des droits pour effectuer cette action !`);
 
 
     switch (interaction.commandName) {
